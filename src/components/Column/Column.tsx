eb8e2293bc7364e3b795c67f18373abf43810cde
Jeremy Cegid <jbeuquila@cegid.com>
2025-05-27 15:18:15 +0200

    fix: add box props to Column component

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Box, { CustomBoxProps } from "../Box";
 
 export interface ColumnProps
-  extends Omit<CustomBoxProps, "display" | "flexDirection"> {
+  extends Omit<CustomBoxProps, "display" | "flexDirection" | "ref"> {
   children: React.ReactNode;
 }
 
-const Column: React.FC<ColumnProps> = ({ children }) => {
+const Column: React.FC<ColumnProps> = (props) => {
+  const { children } = props;
   return (
-    <Box width="100%" display="flex" flexDirection="column">
+    <Box width="100%" display="flex" flexDirection="column" {...props}>
       {children}
     </Box>
   );
