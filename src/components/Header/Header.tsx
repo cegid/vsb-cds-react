97dbe8069656fc43b8df717931c6b697bf32eb1e
Jeremy Cegid <jbeuquila@cegid.com>
2025-06-17 17:14:26 +0200

    fix: remove tonal button for back arrow on header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -284,8 +284,15 @@ const Header: React.FC<HeaderProps> = (props) => {
   return (
     <Row py={5} px={6} gap={4} alignItems="center" width="100%" id={id}>
       {backAction && isMobile && (
-        <IconButton variant="tonal" square onClick={backAction} color="neutral">
-          <Icon size={16}>arrow-left-02</Icon>
+        <IconButton
+          variant="iconOnly"
+          square
+          onClick={backAction}
+          color="neutral"
+        >
+          <Icon variant="stroke" style="rounded" size={16}>
+            arrow-left-02
+          </Icon>
         </IconButton>
       )}
       {!segmentedControlRight && <HeaderTitle title={title} />}
