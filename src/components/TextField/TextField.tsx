c16bd625061d879ca94b3759b7aaf36929a52801
Jeremy Cegid <jbeuquila@cegid.com>
2025-06-18 11:22:10 +0200

    feat: update error text on textfield

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,99 +1,127 @@
-'use client';
+"use client";
 
 import {
   TextField as CegidTextField,
   TextFieldProps as CegidTextFieldProps,
   styled,
-} from '@cegid/cds-react';
+} from "@cegid/cds-react";
 
-import { colorPalettes } from '../../theme/colors';
-import { RADIUS } from '../../theme/radius';
-import typography from '../../theme/typography';
+import { colorPalettes } from "../../theme/colors";
+import { RADIUS } from "../../theme/radius";
+import typography from "../../theme/typography";
+import Row from "../Row";
+import Icon from "../Icon";
+import Typography from "../Typography";
 
 const { primary, neutral, critical } = colorPalettes;
 
 const StyledTextField = styled(CegidTextField)(({ theme, label }) => ({
-  width: '100%',
+  width: "100%",
 
-  '& .MuiInputBase-root': {
-    width: '100%',
+  "& .MuiInputBase-root": {
+    width: "100%",
     ...typography.bodyMRegular,
     borderRadius: RADIUS.S,
     border: `1px solid ${neutral[90]}`,
-    '&:before, &:after': {
-      content: 'none',
+    "&:before, &:after": {
+      content: "none",
     },
-    '&.Mui-error': {
+    "&.Mui-error": {
       borderColor: critical[80],
       backgroundColor: critical[99],
     },
-    '&.Mui-focused': {
+    "&.Mui-focused": {
       outline: `2px solid ${primary[60]}`,
-      outlineOffset: '1px',
+      outlineOffset: "1px",
     },
-    '&.Mui-readOnly': {
+    "&.Mui-readOnly": {
       backgroundColor: neutral[99],
     },
-    '&.MuiInputBase-multiline': {
-      paddingLeft: '12px',
+    "&.MuiInputBase-multiline": {
+      paddingLeft: "12px",
     },
-    [theme.breakpoints.down('sm')]: {
-      '.MuiInputLabel-shrink + .CdsFormItem-inputWrapper &': {
-        paddingTop: '18px',
+    [theme.breakpoints.down("sm")]: {
+      ".MuiInputLabel-shrink + .CdsFormItem-inputWrapper &": {
+        paddingTop: "18px",
       },
     },
   },
 
-  '& .MuiInputAdornment-positionStart': {
-    [theme.breakpoints.down('sm')]: { paddingTop: '24px' },
-    paddingLeft: '12px',
+  "& .MuiInputAdornment-positionStart": {
+    [theme.breakpoints.down("sm")]: { paddingTop: "24px" },
+    paddingLeft: "12px",
   },
 
-  '& .MuiInputBase-input': {
-    padding: '8px 8px 8px 16px',
-    [theme.breakpoints.down('sm')]: {
-      padding: label ? '28px 8px 8px 16px' : '8px 8px 8px 16px',
+  "& .MuiInputBase-input": {
+    padding: "8px 8px 8px 16px",
+    [theme.breakpoints.down("sm")]: {
+      padding: label ? "28px 8px 8px 16px" : "8px 8px 8px 16px",
     },
-    height: '24px',
+    height: "24px",
     color: primary[10],
     ...typography.bodyMRegular,
-    '&::placeholder': {
+    "&::placeholder": {
       color: neutral[50],
       ...typography.bodyMRegular,
     },
   },
 
-  '& .MuiFormLabel-root': {
+  "& .MuiFormLabel-root": {
     color: neutral[50],
     ...typography.bodySSemiBold,
-    marginBottom: '8px',
-    [theme.breakpoints.down('sm')]: {
-      position: 'absolute',
-      top: '10px',
-      left: '15px',
-      backgroundColor: 'transparent',
-      transform: 'translateY(0)',
-      pointerEvents: 'none',
+    marginBottom: "8px",
+    [theme.breakpoints.down("sm")]: {
+      position: "absolute",
+      top: "10px",
+      left: "15px",
+      backgroundColor: "transparent",
+      transform: "translateY(0)",
+      pointerEvents: "none",
       zIndex: 1,
     },
-    '&.MuiInputLabel-shrink': {
-      [theme.breakpoints.down('sm')]: {
-        transform: 'translateY(0) scale(0.85)',
-        transformOrigin: 'top left',
+    "&.MuiInputLabel-shrink": {
+      [theme.breakpoints.down("sm")]: {
+        transform: "translateY(0) scale(0.85)",
+        transformOrigin: "top left",
       },
     },
-    '& .MuiInputLabel-asterisk': {
-      color: 'inherit',
+    "& .MuiInputLabel-asterisk": {
+      color: "inherit",
     },
   },
-
-  '&.CdsFormItem': {
-    width: '100%',
+  "&.CdsFormItem": {
+    width: "100%",
+  },
+  "& .CdsErrorText-errorMessage": {
+    display: "none !important",
+  },
+  '& [id$="-error-text"]': {
+    display: "none !important",
+  },
+  '& [aria-live="polite"]': {
+    display: "none !important",
+  },
+  "& .mui-qaiqkx": {
+    display: "none !important",
   },
 }));
 
 const TextField = (props: CegidTextFieldProps) => {
-  return <StyledTextField {...props} />;
+  return (
+    <>
+      <StyledTextField {...props} />
+      {props.errorText && (
+        <Row gap={2} pt={4}>
+          <Icon variant="bulk" size={16} color="critical/50">
+            spam
+          </Icon>
+          <Typography variant="captionRegular" color="critical/50">
+            {props.errorText}
+          </Typography>
+        </Row>
+      )}
+    </>
+  );
 };
 
 export default TextField;
