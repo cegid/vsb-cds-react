1e781e33b13b64ca932420ed74ff6e6521aedbbb
jeremy cegid <jbeuquila@cegid.com>
2025-06-30 11:52:18 +0200

    fix: padding on multiline textfield

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -17,100 +17,108 @@ export interface TextFieldProps extends CegidTextFieldProps {}
 
 const { primary, neutral, critical } = colorPalettes;
 
-const StyledTextField = styled(CegidTextField)(({ theme, label }) => ({
-  width: "100%",
-
-  "& .MuiInputBase-root": {
+const StyledTextField = styled(CegidTextField)(
+  ({ theme, label, multiline }) => ({
     width: "100%",
-    ...typography.bodyMRegular,
-    borderRadius: RADIUS.S,
-    border: `1px solid ${neutral[90]}`,
-    "&:before, &:after": {
-      content: "none",
-    },
-    "&.Mui-error": {
-      borderColor: critical[80],
-      backgroundColor: critical[99],
-    },
-    "&.Mui-focused": {
-      outline: `2px solid ${primary[70]}`,
-      outlineOffset: "1px",
+
+    "& .MuiInputBase-root": {
+      width: "100%",
+      ...typography.bodyMRegular,
+      borderRadius: RADIUS.S,
+      border: `1px solid ${neutral[90]}`,
+      "&:before, &:after": {
+        content: "none",
       },
-    "&.Mui-readOnly": {
-      backgroundColor: neutral[99],
+      "&.Mui-error": {
+        borderColor: critical[80],
+        backgroundColor: critical[99],
+      },
       "&.Mui-focused": {
-        outline: "none",
-        outlineOffset: "0",
+        outline: `2px solid ${primary[70]}`,
+        outlineOffset: "1px",
+      },
+      "&.Mui-readOnly": {
+        backgroundColor: neutral[99],
+        "&.Mui-focused": {
+          outline: "none",
+          outlineOffset: "0",
+        },
+      },
+      "&.MuiInputBase-multiline": {
+        paddingLeft: "12px",
+      },
+      [theme.breakpoints.down("sm")]: {
+        ".MuiInputLabel-shrink + .CdsFormItem-inputWrapper &": {
+          paddingTop: "18px",
+        },
       },
     },
-    "&.MuiInputBase-multiline": {
+
+    "& .MuiInputAdornment-positionStart": {
+      [theme.breakpoints.down("sm")]: { paddingTop: "24px" },
       paddingLeft: "12px",
     },
-    [theme.breakpoints.down("sm")]: {
-      ".MuiInputLabel-shrink + .CdsFormItem-inputWrapper &": {
-        paddingTop: "18px",
-      },
-    },
-  },
 
-  "& .MuiInputAdornment-positionStart": {
-    [theme.breakpoints.down("sm")]: { paddingTop: "24px" },
-    paddingLeft: "12px",
-  },
-
-  "& .MuiInputBase-input": {
-    padding: "8px 8px 8px 16px",
-    [theme.breakpoints.down("sm")]: {
-      padding: label ? "28px 8px 8px 16px" : "8px 8px 8px 16px",
-    },
-    height: "24px",
-    color: neutral[10],
-    ...typography.bodyMRegular,
-    "&::placeholder": {
-      color: neutral[50],
-      ...typography.bodyMRegular,
-    },
-  },
-
-  "& .MuiFormLabel-root": {
-    color: neutral[50],
-    ...typography.bodySSemiBold,
-    marginBottom: "8px",
-    [theme.breakpoints.down("sm")]: {
-      position: "absolute",
-      top: "10px",
-      left: "15px",
-      backgroundColor: "transparent",
-      transform: "translateY(0)",
-      pointerEvents: "none",
-      zIndex: 1,
-    },
-    "&.MuiInputLabel-shrink": {
+    "& .MuiInputBase-input": {
+      padding: multiline ? "8px 8px 8px 0" : "8px 8px 8px 16px",
       [theme.breakpoints.down("sm")]: {
-        transform: "translateY(0) scale(0.85)",
-        transformOrigin: "top left",
+        padding: multiline
+          ? label
+            ? "28px 8px 8px 0"
+            : "8px 8px 8px 0"
+          : label
+          ? "28px 8px 8px 16px"
+          : "8px 8px 8px 16px",
+      },
+      height: "24px",
+      color: neutral[10],
+      ...typography.bodyMRegular,
+      "&::placeholder": {
+        color: neutral[50],
+        ...typography.bodyMRegular,
       },
     },
-    "& .MuiInputLabel-asterisk": {
-      color: "inherit",
+
+    "& .MuiFormLabel-root": {
+      color: neutral[50],
+      ...typography.bodySSemiBold,
+      marginBottom: "8px",
+      [theme.breakpoints.down("sm")]: {
+        position: "absolute",
+        top: "10px",
+        left: "15px",
+        backgroundColor: "transparent",
+        transform: "translateY(0)",
+        pointerEvents: "none",
+        zIndex: 1,
+      },
+      "&.MuiInputLabel-shrink": {
+        [theme.breakpoints.down("sm")]: {
+          transform: "translateY(0) scale(0.85)",
+          transformOrigin: "top left",
+        },
+      },
+      "& .MuiInputLabel-asterisk": {
+        color: "inherit",
+      },
     },
-  },
-  "&.CdsFormItem": {
-    width: "100%",
-  },
-  "& .CdsErrorText-errorMessage": {
-    display: "none !important",
-  },
-  '& [id$="-error-text"]': {
-    display: "none !important",
-  },
-  '& [aria-live="polite"]': {
-    display: "none !important",
-  },
-  "& .mui-qaiqkx": {
-    display: "none !important",
-  },
-}));
+    "&.CdsFormItem": {
+      width: "100%",
+    },
+    "& .CdsErrorText-errorMessage": {
+      display: "none !important",
+    },
+    '& [id$="-error-text"]': {
+      display: "none !important",
+    },
+    '& [aria-live="polite"]': {
+      display: "none !important",
+    },
+    "& .mui-qaiqkx": {
+      display: "none !important",
+    },
+  })
+);
 
 const TextField = (props: CegidTextFieldProps) => {
   return (
