6c244a37b45383d61833ee07c2e21c6933eae961
jeremy cegid <jbeuquila@cegid.com>
2025-11-12 14:02:13 +0100

    feat(Table): add bordered props to add border on table content

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -7,10 +7,18 @@ import {
   TableProps as CegidTableProps,
 } from "@cegid/cds-react";
 import typography from "../../theme/typography";
+import { borderNeutral } from "@cegid/vsb-cds-tokens";
+import { forwardRef } from "react";
 
-export interface TableProps extends CegidTableProps {}
+export interface TableProps extends CegidTableProps {
+  /**
+   * Si true, affiche une bordure autour du tableau avec des coins arrondis
+   * @default false
+   */
+  bordered?: boolean;
+}
 
-export const Table = styled(CegidTable)({
+const StyledTable = styled(CegidTable)({
   "& .MuiTableCell-root": {
     border: "none",
     background: "transparent !important",
@@ -34,6 +42,47 @@ export const Table = styled(CegidTable)({
   },
 });
 
+export const Table = forwardRef<HTMLTableElement, TableProps>(
+  ({ bordered = false, sx, ...props }, ref) => {
+    const borderedStyles = bordered
+      ? {
+          border: "1px solid",
+          borderColor: borderNeutral,
+          borderCollapse: "separate" as const,
+          borderSpacing: 0,
+          borderRadius: "16px",
+          overflow: "hidden",
+          "& .MuiTableHead-root .MuiTableRow-root:first-of-type": {
+            "& .MuiTableCell-root:first-of-type": {
+              borderTopLeftRadius: "16px",
+            },
+            "& .MuiTableCell-root:last-of-type": {
+              borderTopRightRadius: "16px",
+            },
+          },
+          "& .MuiTableFooter-root .MuiTableRow-root:last-of-type": {
+            "& .MuiTableCell-root:first-of-type": {
+              borderBottomLeftRadius: "16px",
+            },
+            "& .MuiTableCell-root:last-of-type": {
+              borderBottomRightRadius: "16px",
+            },
+          },
+        }
+      : {};
+
+    return (
+      <StyledTable
+        ref={ref}
+        sx={{
+          ...borderedStyles,
+          ...sx,
+        }}
+        {...props}
+      />
+    );
+  }
+);
 
 Table.displayName = "Table";
 
